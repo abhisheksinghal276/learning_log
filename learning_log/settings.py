26c84ba5afdf59a65ced2fb9e78019987db9b3b4
abhisheksinghal276 <abhisheksinghal276@gmail.com>
2024-12-19 14:02:03 +0530

    Made custom 404 and 500 pages

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -50,7 +50,7 @@ if os.getcwd() == '/app':
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -84,7 +84,7 @@ ROOT_URLCONF = 'learning_log.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'learning_log/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
