c0c03572245c256de81764b2e458b95a3993d9b1
abhisheksinghal276@gmail.com <abhisheksinghal276@gmail.com>
2025-02-14 14:21:00 +0530

    Updated ALLOWED HOSTS

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "django-insecure-x7_yr7nl*rsialj8$6y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost 127.0.0.1").split(",")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost 127.0.0.1 learning-log-1-a398566035da.herokuapp.com").split(",")
 
 
 # Application definition
